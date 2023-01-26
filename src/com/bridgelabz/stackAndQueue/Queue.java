e69655340c3eb058b24ea7aa13418c43ef9e3044
komalarao <manoharvakamullu1999@gmail.com>
2023-01-26 10:07:21 -0800

    refactor: program to dequeue elements from the queue

--- src/com/bridgelabz/stackAndQueue/Queue.java
+++ src/com/bridgelabz/stackAndQueue/Queue.java
@@ -3,8 +3,15 @@ package com.bridgelabz.stackAndQueue;
 public class Queue {
 	LinkedList list = new LinkedList(); // Declaring linked list
 
+	/*
+	 * Creating Queue method
+	 */
 	public void enQueue(Object data) {
 		list.addFirst(data);
+	}
+
+	public void deQueue() {
+		list.deleteFirst();
 	}
 
 	// display method
@@ -13,11 +20,18 @@ public class Queue {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("welcome to Queue program");
+		System.out.println("Welcome to Stack and Queue Data structure");
 		Queue queue = new Queue();
 		queue.enQueue(70);
 		queue.enQueue(30);
 		queue.enQueue(56);
+		queue.display();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
 		queue.display();
 	}
 
